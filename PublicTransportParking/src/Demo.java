5dad8fa78540fcd1ade1954fd26cccf7d3fb0ce7
ivanrezic <ivan.rezic1@gmail.com>
2019-01-12 13:12:13 +0100

    u mainu sam maka ovih par streamove za blokirane trake jer je sam vec u Data class implementira metode koje vracaju to

--- PublicTransportParking/src/Demo.java
+++ PublicTransportParking/src/Demo.java
@@ -9,25 +9,18 @@ public class Demo {
 
         List<Vehicle> vehicles = data.getAllVehicles();
         List<Track> tracks = data.getAllTracks();
-        List<Track> bTracks = tracks.stream().filter(track -> !track.trakeKojeOvaTrakaBlokira.isEmpty()).collect(Collectors.toList());
-        List<Integer> blockingTracks = tracks.stream().filter(track -> !track.trakeKojeOvaTrakaBlokira.isEmpty()).map(t -> t.idTrake).collect(Collectors.toList());
-        List<Integer> blockedTracks = tracks.stream().filter(
-                track -> bTracks.stream().flatMap(tr ->
-                        tr.trakeKojeOvaTrakaBlokira.stream()).collect(Collectors.toList()).contains(track.idTrake)).map(track -> track.idTrake).collect(Collectors.toList());
 
-        blockingTracks.forEach(System.out::println);
-        System.out.println("");
-        blockedTracks.forEach(System.out::println);
 
-        tracks.forEach(System.out::println);
+//        tracks.forEach(System.out::println);
 //        vehicles.forEach(System.out::println);
-        for (int i = 0; i< 20; i++) {
-       //     Schedule sc = new Schedule(tracks, vehicles, blockingTracks, blockedTracks);
-      //      System.out.println(sc.firstGlobalGoal());
 
-        }
-      //  Schedule sc = new Schedule(tracks, vehicles, blockingTracks, blockedTracks);
-      //  System.out.println(sc);
+        Schedule schedule = new Schedule(
+                tracks,
+                vehicles,
+                data.getTracksThatBlockOtherTracks(),
+                data.getTracksBlockedByOtherTracks());
+
+        System.out.println(schedule.firstGlobalGoal());
 
     }
 }
